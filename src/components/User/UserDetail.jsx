d3ef9da75437708ed3ad077056b52f3e37063151
lumagc <lual.30xd@gmail.com>
2022-07-27 16:35:34 +0200

    Add styles

--- src/components/User/UserDetail.jsx
+++ src/components/User/UserDetail.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import { Link, useLocation } from "react-router-dom";
+import { useLocation } from "react-router-dom";
+import "./User.css";
 
 const UserDetail = () => {
     const location = useLocation();
@@ -48,31 +49,44 @@ const UserDetail = () => {
     }
 
     return <section>
-        <h1>Datos del usuario</h1>
+        <h1 className="h1">User data</h1>
+        <div className="list">
         {
           userData.map((user, index) => (
-            <div key={index}>
-              <p>{user.name}</p>
-              <p>{user.username}</p>
-              <p>{user.email}</p>
-              <p>{user.address.street}</p>
-              <p>{user.address.suite}</p>
-              <p>{user.address.city}</p>
-              <p>{user.address.zipcode}</p>
-              <p>{user.phone}</p>
-              <p>{user.website}</p>
-              <p>{user.company.name}</p>
-              <p>{user.catchPhrase}</p>
-              <p>{user.bs}</p>
-              <h1>Change email and phone</h1>
-              <form onSubmit={handleSubmit} encType="multipart/form-data">
-                <input type="email" name="email" id="email" value={userEmail} onChange={(ev) => setUserEmail(ev.target.value)} />
-                <input type="text" name="phone" id="phone" value={userPhone} onChange={(ev) => setUserPhone(ev.target.value)}/>
-                <button type="submit">Editar</button>
-              </form>
-            </div>
+            <ul className="list-group" key={index}>
+              <li className="list-group-item"><b>Name:</b> {user.name}</li>
+              <li className="list-group-item"><b>Username:</b> {user.username}</li>
+              <li className="list-group-item"><b>Email:</b> {user.email}</li>
+              <ul className="list-group">
+                <li className="list-group-item">Address</li>
+                <li className="list-group-item"><b>Street:</b> {user.address.street}</li>
+                <li className="list-group-item"><b>Suite:</b> {user.address.suite}</li>
+                <li className="list-group-item"><b>City:</b> {user.address.city}</li>
+                <li className="list-group-item"><b>Zipcode:</b> {user.address.zipcode}</li>
+              </ul>
+              <li className="list-group-item"><b>Phone:</b> {user.phone}</li>
+              <li className="list-group-item"><b>Website</b> {user.website}</li>
+              
+              <ul className="list-group">
+                <li className="list-group-item">Company</li>
+                <li className="list-group-item"><b>Name:</b> {user.company.name}</li>
+                <li className="list-group-item"><b>CatchPhrase:</b> {user.company.catchPhrase}</li>
+                <li className="list-group-item"><b>bs:</b> {user.company.bs}</li>
+              </ul>
+            </ul>
           ))
         }
+        </div>
+        <div className="container">
+          <h3>Change email and phone</h3>
+          <form onSubmit={handleSubmit} encType="multipart/form-data">
+          <label htmlFor="email">Email:</label><br />
+          <input className="form-control" type="email" name="email" id="email" value={userEmail} onChange={(ev) => setUserEmail(ev.target.value)} /> <br />
+          <label htmlFor="phone">Phone:</label><br />
+          <input className="form-control" type="text" name="phone" id="phone" value={userPhone} onChange={(ev) => setUserPhone(ev.target.value)}/> <br />
+          <button className="btn btn-primary" type="submit">Update</button>
+          </form>
+        </div>
     </section>
 }
 
--- src/components/User/UserList.jsx
+++ src/components/User/UserList.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import {Link} from 'react-router-dom';
+import "./User.css";
 
 const UserList = () => {
     const [usersData, setUsersData] = useState([]);
@@ -32,26 +33,41 @@ const UserList = () => {
       )
     }
 
-    console.log(usersData.filter(user=>user.name.toLowerCase().includes("Ervin")));
-
     return <section>
-      <h1>Lista de usuarios</h1>
-      <input type="text" placeholder="filter...." onChange={(ev) => setSearchName(ev.target.value) } />
-      <div>
-        {
-          searchFilter().map((user, index) => (
-            <ul key={index}>
-              <li>{user.id}</li>
-              <li>{user.name}</li>
-              <li>{user.username}</li>
-              <li>{user.email}</li>
-              <li>{user.phone}</li>
-              <Link to={`/userDetail/${user.id}`}>
-                <button>Ver mas detalles</button>
-              </Link>
-            </ul>
-          ))
-        }
+      <h1 className="h1">User list</h1>
+      <div className="container">
+        <input className="form-control" type="text" id="search" placeholder="filter...." onChange={(ev) => setSearchName(ev.target.value) } />
+      </div>
+      <div className="container-table">
+        <table className="table table-bordered">
+          <caption>Users</caption>
+          <tbody>
+            <tr>
+              <th scope="col">Id</th>
+              <th scope="col">Name</th>
+              <th scope="col">Username</th>
+              <th scope="col">Email</th>
+              <th scope="col">Phone</th>
+              <th scope="col">More details</th>
+            </tr>
+            {
+              searchFilter().map((user) => (
+                <tr key={user.id}>
+                  <th scope="row">{user.id}</th>
+                  <td>{user.name}</td>
+                  <td>{user.username}</td>
+                  <td>{user.email}</td>
+                  <td>{user.phone}</td>
+                  <td>
+                    <Link to={`/userDetail/${user.id}`}>
+                      <button className="btn btn-info">See more</button>
+                    </Link>
+                  </td>
+                </tr>
+              ))
+            }
+          </tbody>
+        </table>
       </div>
     </section>
 }
